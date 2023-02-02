b310c13a559e3fca846373fc64c33a35de67775c
Bhushan0246 <65452015+Bhushan0246@users.noreply.github.com>
2023-02-02 18:06:33 +0530

    Add files via upload

---
+++ average with fun.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+float my_average(float *a,float *b){
+	float ave;
+	ave=((*a) + (*b))/2;
+	return ave;
+}
+int main(){
+	float a, b, average;
+	printf("Enter the value of first number : ");
+	scanf("%f",&a);
+	printf("Enter the value of second number : ");
+	scanf("%f",&b);
+	average=my_average(&a, &b);
+	printf("The average of two numbers is : %f",average);
+	return 0;
+}
