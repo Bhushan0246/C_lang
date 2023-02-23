eddf09952a7b85e2194b45aee5f3b85fb13928f2
Bhushan0246 <65452015+Bhushan0246@users.noreply.github.com>
2023-02-23 14:05:34 +0530

    Add files via upload

---
+++ memory allocation.c
@@ -0,0 +1,19 @@
+#include<stdlib.h>
+int main(){
+	int n, i, *ptr, sum = 0;
+	printf("Enter the number of elements : ");
+	scanf("%d",&n);
+	ptr=(int*)malloc(n*sizeof(double));	//allocating memory
+	if(ptr == NULL){
+		printf("Sorry, Memory can not be allocated.");
+		exit(0);
+	}
+	for(i=0;i<n;i++){
+		printf("Enter element of array : ");
+		scanf("%d", (ptr+i));
+		sum += *(ptr+i);
+	}
+	printf("\nSum = %d", sum);
+	free(ptr);
+	return 0;
+}
