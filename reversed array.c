9d99d16ac87e84aaa172c7481a75e43e6a06406b
Bhushan0246 <65452015+Bhushan0246@users.noreply.github.com>
2023-05-04 22:58:46 +0530

    Add files via upload

--- reversed array.c
+++ reversed array.c
@@ -1,22 +1,18 @@
 #include<stdio.h>
 int main(){
 	int i, size;
-	printf("Enter size of array : ");
+	printf("Size of array: ");
 	scanf("%d", &size);
 	int arr[size];
-	puts("Enter elements of array: ");
-	for (i=0; i<size; i++){
+	printf("Enter elements of array: ");
+	for(i=0; i<size; i++){
 		scanf("%d", &arr[i]);
 	}
-	system("CLS");
-	puts("Original array: ");
-	for (i=0; i<size; i++){
-		printf("%d ", arr[i]);
+	printf("Reversed array: { ");
+	for(i=size-1; i>=0; i--){
+		printf("%d ",arr[i]);
 	}
-	puts("Reversed array : ");
-	for (i=size-1; i>=0; i--){
-		printf("%d ", arr[i]);
-	}
+	puts("}");
 	
 	return 0;
 }
---
+++ searching in array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+	int i, x, size;
+	printf("Size of array: ");
+	scanf("%d", &size);
+	int arr[size];
+	printf("Enter elements: ");
+	for(i=0; i<size; i++){
+		scanf("%d", &arr[i]);
+	}
+	printf("Which element you want to search? ");
+	scanf("%d", &x);
+	for(i=0; i<size; i++){
+		if(arr[i] == x){
+			printf("%d is at index[%d]", x, i);
+		}
+	}
+	return 0;
+}
+
