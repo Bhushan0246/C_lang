2644d562f89c495a1400e979af17c33decac401e
Bhushan0246 <65452015+Bhushan0246@users.noreply.github.com>
2023-05-09 10:37:24 +0530

    Update left star triangle ptn.c

--- left star triangle ptn.c
+++ left star triangle ptn.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main(){
-    int n;
+    int i, j, n;
     printf("Enter the number of rows : ");
     scanf("%d",&n);
-    for(int i=1;i<n;i++){
-        for(int j=1;j<=i;j++){
+    for(i=0;i<n;i++){
+        for(j=0;j<=i;j++){
             printf("* ");
     }
         printf("\n");
